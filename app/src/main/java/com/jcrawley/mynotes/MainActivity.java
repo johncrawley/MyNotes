2d3d8cb269448fb1232d115193949fdece6faea5
johncrawley <johncrawley1@gmail.com>
2021-09-29 22:23:16 +0100

    Added list with material edit text to main activity.

--- app/src/main/java/com/jcrawley/mynotes/MainActivity.java
+++ app/src/main/java/com/jcrawley/mynotes/MainActivity.java
@@ -2,13 +2,42 @@ package com.jcrawley.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupInputText();
     }
+
+
+    private void setupInputText(){
+        EditText addCategoryEditText = findViewById(R.id.addCategoryEditText);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        addCategoryEditText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+
+                v.getText();
+                addCategoryEditText.getText().clear();
+                imm.hideSoftInputFromWindow(addCategoryEditText.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        });
+    }
+
+
 }

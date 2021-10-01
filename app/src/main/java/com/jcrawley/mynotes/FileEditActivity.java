eb96f56f54f71223a1d0caba02f5acf411997800
johncrawley <johncrawley1@gmail.com>
2021-10-01 23:26:04 +0100

    Added loading and saving data from internal file.

--- app/src/main/java/com/jcrawley/mynotes/FileEditActivity.java
+++ app/src/main/java/com/jcrawley/mynotes/FileEditActivity.java
@@ -2,13 +2,50 @@ package com.jcrawley.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.jcrawley.mynotes.list.ListItem;
+import com.jcrawley.mynotes.repository.FileHandler;
 
 public class FileEditActivity extends AppCompatActivity {
+
+
+    private final String testCategoryName="testCategory";
+    private final String testFileName= "test";
+
+    FileHandler fileHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_edit);
+        fileHandler = new FileHandler(this);
+        String contents = fileHandler.readFileFromInternalStorage(testCategoryName, testFileName);
+        EditText editText = findViewById(R.id.editTextFileContents);
+        editText.setText(fileHandler.getFileContents());
+        setupInputText();
     }
+
+
+    private void setupInputText(){
+        EditText fileContentsEditText = findViewById(R.id.editTextFileContents);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        fileContentsEditText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                String contents = v.getText().toString().trim();
+                //fileHandler.writeFileOnInternalStorage(testCategoryName, testFileName, contents);
+                fileHandler.saveFileContents();
+                imm.hideSoftInputFromWindow(fileContentsEditText.getWindowToken(), 0);
+                return true;
+            }
+            return false;
+        });
+    }
+
 }
---
+++ app/src/main/java/com/jcrawley/mynotes/repository/FileHandler.java
@@ -0,0 +1,167 @@
+package com.jcrawley.mynotes.repository;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileHandler {
+
+    private final File baseDirectory;
+    private final Context context;
+    private String filePath;
+
+    public FileHandler(Context context){
+        this.context = context;
+        final String DIRECTORY_NAME = "fileDir";
+        baseDirectory = new File(context.getFilesDir(), DIRECTORY_NAME);
+    }
+
+    private void createFile2(String categoryName, String filename){
+        File file2 = new File(context.getFilesDir(), getFullFilename(categoryName, filename));
+        File file = new File(context.getApplicationContext().getFilesDir(), getFullFilename(categoryName, filename));
+
+        if(file.exists()){
+            System.out.println("^^^ File already exists : " + file.getAbsolutePath());
+            return;
+        }
+        try {
+            System.out.println("^^^ About to create file: " + file.getAbsolutePath());
+            file.createNewFile();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void createFile(String filename){
+        FileOutputStream outputStream = null;
+        byte [] bytes = new byte[]{};
+        try {
+            outputStream = context.openFileOutput(filename, Context.MODE_APPEND);
+            outputStream.write(bytes);
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void writeFileOnInternalStorage(String categoryName, String filename, String str){
+
+        String fullFilename = getFullFilename(categoryName, filename);
+
+        createFile(fullFilename);
+        filePath = context.getApplicationContext().getFilesDir() + fullFilename;
+        if(!couldNotCreateBaseDirectory()){
+            return;
+        }
+        try {
+            FileOutputStream fileOutputStream = context.getApplicationContext().openFileOutput(fullFilename,Context.MODE_PRIVATE);
+            //FileWriter writer = new FileWriter(filePath);
+
+               OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream);
+                writer.write(str);
+                writer.flush();
+                writer.flush();
+                writer.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private String getFullFilename(String categoryName, String filename){
+        String EXTENSION = ".txt";
+        return categoryName + "__" + filename  + EXTENSION;
+    }
+
+
+    public String readFileFromInternalStorage(String categoryName, String filename){
+
+        StringBuilder text = new StringBuilder();
+        try {
+            filePath = context.getFilesDir() + getFullFilename(categoryName, filename);
+            printFileInfo(filePath);
+           // FileInputStream fileInputStream = context.getApplicationContext().openFileInput(filePath);
+            FileReader fileReaderold = new FileReader(new File(filePath));
+            FileReader fileReader = new FileReader(new File(getFullFilename(categoryName, filename)));
+            //InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+            BufferedReader buffer = new BufferedReader(fileReader);
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            buffer.close();
+        } catch (IOException e) {
+            System.out.println("^^^ Error! occured while reading text file from Internal Storage!");
+            e.printStackTrace();
+        }
+        return text.toString();
+    }
+
+
+
+    String filename = "myfile";
+    String fileContents = "Hello world!";
+
+    public String getFileContents(){
+        File file = new File(context.getFilesDir(), filename);
+        StringBuilder str = new StringBuilder();
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader buffer = new BufferedReader(fileReader);
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                str.append(line);
+                str.append('\n');
+            }
+            buffer.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return str.toString();
+    }
+
+
+    public void saveFileContents(){
+        fileContents += " " + System.currentTimeMillis();
+        try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
+            fos.write(fileContents.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    private  void printFileInfo(String filePath){
+        File file = new File(filePath);
+        System.out.println("^^^ Filepath: " + filePath + " exists? : " + file.exists());
+    }
+
+
+    private boolean couldNotCreateBaseDirectory(){
+        if(baseDirectory.exists()){
+           return !baseDirectory.mkdir();
+        }
+        return false;
+    }
+
+}
